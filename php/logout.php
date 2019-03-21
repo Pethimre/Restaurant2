44207ec493f075b2c4d080a6e23f13e713b170b3
Pethimre <petho.9884@gmail.com>
2019-03-21 13:16:53 +0100

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,12 +1,6 @@
 <?php
-<<<<<<< HEAD
-	session_start();
-	session_unset();
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-=======
 session_start();
 session_unset();
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
->>>>>>> Expiremental
 ?>
